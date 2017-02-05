47af839d7d6afe24f27c9c5b469fb3120b1b320a
Parshwa Shah <parshwa.surat@gmail.com>
2017-02-05 20:23:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def handle_messages():
                     elif message_text[0] == '!':
                         message_text = message_text[1:]
                         send_message(sender_id, message_text[::-1])
-                    elif message_text[:5] == '@wiki':
-                        message_text = message_text[6:]
-                        result_search = wikipedia.search(message_text)
-                        if result_search == []:
-                            send_message(sender_id, "Sorry, We were unable to find " + message_text)
+                   # elif message_text[:5] == '@wiki':
+                        #message_text = message_text[6:]
+                        #result_search = wikipedia.search(message_text)
+                        #if result_search == []:
+                        #    send_message(sender_id, "Sorry, We were unable to find " + message_text)
                         else:
                             final_result = wikipedia.summary(result_search[0], sentences=3)
                             send_message(sender_id, final_result)
